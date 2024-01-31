6ef6908a13ab1c02fc5e24043d88e6c80f155c61
GustavoS11 <gustavohenrique6224@gmail.com>
2024-01-31 00:04:05 -0300

    função para redirecionar usuário para edição de nome

--- dados/script.js
+++ dados/script.js
@@ -1,1 +1,9 @@
-const inputName = document.querySelector("#input-name")
+const inputSubmitName = document.querySelector("#input-submitName")
+console.log("tete")
+inputSubmitName.addEventListener("click", (event) => {
+    event.preventDefault()
+    redirect()
+})
+async function redirect() {
+    window.location.href = "../editar-dados/editar-nome/index.html"
+}
