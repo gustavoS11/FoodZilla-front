653e342aacd25418de046f6a62997525e4d4a8d9
JoaoGold <joaogold210304@gmail.com>
2024-02-02 20:08:44 -0300

    script de cadastro realizado

--- register/script.js
+++ register/script.js
@@ -2,15 +2,16 @@ const myHeaders = {
     "Content-Type": "application/json"
 }
 async function registerUser(dados) {
-    console.log("sim")
+    console.log(dados)
     const dadosJson = JSON.stringify(dados)
     const register = await fetch(`http://localhost:3000/user/register`, {
         method: 'POST',
         body: dadosJson,
         headers: myHeaders
     })
-    if (registerUser.status == 200) {
-        registerUserJson = await registerUser.json()
+    console.log(register)
+    if (register.status == 200) {
+        registerUserJson = await register.json()
         toastify("Ok, login efetuado com sucesso!", "ok")
         localStorage.setItem("@token-exemplo", resJson.accessToken)
         localStorage.setItem("@user-exemplo", JSON.stringify(resJson.user))
@@ -24,6 +25,12 @@ async function registerUser(dados) {
 const submit = document.querySelector(".input-submit")
 submit.addEventListener("click", (event) => {
     event.preventDefault()
-    const dados = document.querySelector("body")
+    const nome = document.querySelector("#input-name").value
+    const email = document.querySelector("#input-email").value
+    const senha = document.querySelector("#input-password").value
+    const id_tipo_usuario = 1
+    const dados = {
+        id_tipo_usuario,nome,email,senha
+    }
     registerUser(dados)
 })
