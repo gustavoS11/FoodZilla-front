ae8471bb15ff761d09c3fdb03f4aa5954d9a7f26
JoaoGold <joaogold210304@gmail.com>
2024-02-02 21:53:07 -0300

    mudanças nos scripts

--- dados/script.js
+++ dados/script.js
@@ -1,4 +1,36 @@
 const inputSubmitName = document.querySelector("#input-submitName")
+const inputSubmitEmail = document.querySelector("#input-submitEmail")
+const inputSubmitSenha = document.querySelector("#input-submitPassword")
+const inputSubmitProfile = document.querySelector("#input-submitProfile")
+
+const inputName = document.querySelector("#input-name")
+const inputPassword = document.querySelector("#input-password")
+const inputEmail = document.querySelector("#input-email")
+
+async function listData() {
+    const products = await fetch("http://localhost:3000/user/list")
+    const productsJson = await products.json()
+    const div = document.querySelector("#div-products")
+    productsJson.forEach(element => {
+        const price = element.preco
+        const preco = price.toString().replace(".", ",")
+        div.insertAdjacentHTML("beforeend",`
+        <ul> 
+            <li><img class="img-li" src="${element.url}" alt="${element.nome}"></li>
+            <li>
+                <h2>${element.nome}</h2>
+                <h3>R$${preco}</h3>
+                <label for="input-li">${element.descricao}</label>
+                <br>
+                <input class="input-submit" name="input-li" type="submit" value="Comprar">
+            </li>
+        </ul>`
+        )
+    });
+}
+listProducts()
+
+
 inputSubmitName.addEventListener("click", (event) => {
     event.preventDefault()
     redirectName()
@@ -6,7 +38,6 @@ inputSubmitName.addEventListener("click", (event) => {
 async function redirectName() {
     window.location.href = "../editar-dados/editar-nome/index.html"
 }
-const inputSubmitEmail = document.querySelector("#input-submitEmail")
 inputSubmitEmail.addEventListener("click", (event) => {
     event.preventDefault()
     redirectEmail()
@@ -14,7 +45,6 @@ inputSubmitEmail.addEventListener("click", (event) => {
 async function redirectEmail() {
     window.location.href = "../editar-dados/editar-email/index.html"
 }
-const inputSubmitSenha = document.querySelector("#input-submitPassword")
 inputSubmitSenha.addEventListener("click", (event) => {
     event.preventDefault()
     redirectSenha()
@@ -22,15 +52,9 @@ inputSubmitSenha.addEventListener("click", (event) => {
 async function redirectSenha() {
     window.location.href = "../editar-dados/editar-senha/index.html"
 }
-const inputSubmitNumber = document.querySelector("#input-submitNumber")
-inputSubmitNumber.addEventListener("click", (event) => {
-    event.preventDefault()
-    redirectNumero()
-})
 async function redirectNumero() {
     window.location.href = "../editar-dados/editar-contato/index.html"
 }
-const inputSubmitProfile = document.querySelector("#input-submitProfile")
 inputSubmitProfile.addEventListener("click", (event) => {
     event.preventDefault()
     redirectProfile()
